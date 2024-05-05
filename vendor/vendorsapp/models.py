c62af9c1df553aa589204cc114be0e17857899af
Moniska <patelm2606@gmail.com>
2024-05-05 21:42:25 +0530

    models

--- vendor/vendorsapp/models.py
+++ vendor/vendorsapp/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils import timezone
+from django.db.models import Count, Avg
+from django.db.models import F
 # Create your models here.
 
 
@@ -48,20 +50,44 @@ class HistoricalPerformance(models.Model):
     
 
 #Backend Logic
-# @receiver(post_save, sender=PurchaseOrder)
-# def calculate_on_time_delivery_rate(sender, instance, created, **kwargs):
-#     if instance.status == 'completed':
-#         # Logic to calculate on-time delivery rate...
+@receiver(post_save, sender=PurchaseOrder)
+def update_vendor_performance(sender, instance, **kwargs):
+    if instance.status == 'completed' and instance.delivered_data is None:
+        instance.delivered_data = timezone.now()
+        instance.save()
 
-# @receiver(post_save, sender=PurchaseOrder)
-# def update_quality_rating_average(sender, instance, created, **kwargs):
-#     if instance.quality_rating is not None:
-#         # Logic to update quality rating average...
+    #
+    completed_orders = PurchaseOrder.objects.filter(vendor=instance.vendor, status='completed')
+    # on_time_delivery_rate = completed_orders.filter(delivery_date__lte=instance.delivery_date).count() / completed_orders.count()
+    on_time_deliveries = completed_orders.filter(delivery_date__gte=F('delivered_data'))
+    on_time_delivery_rate = on_time_deliveries.count() / completed_orders.count()
+    instance.vendor.on_time_delivery_rate = on_time_delivery_rate if on_time_delivery_rate else 0
 
-# @receiver(post_save, sender=PurchaseOrder)
-# def calculate_average_response_time(sender, instance, created, **kwargs):
-#     if instance.acknowledgment_date:
-#         # Logic to calculate average response time...
+    # Update Quality Rating Average
+    completed_orders_with_rating = completed_orders.exclude(quality_rating__isnull=True)
+    quality_rating_avg = completed_orders_with_rating.aggregate(Avg('quality_rating'))['quality_rating__avg'] or 0
+    instance.vendor.quality_rating_avg = quality_rating_avg if quality_rating_avg else 0
+    instance.vendor.save()
 
-# @receiver(post_save, sender=PurchaseOrder)
-# def calculate_fulfillment_rate(sender, instance, created, **kwargs):
+@receiver(post_save, sender=PurchaseOrder)
+def update_response_time(sender, instance, **kwargs):
+    # if instance.acknowledgment_date:
+        # Update Average Response Time
+        response_times = PurchaseOrder.objects.filter(vendor=instance.vendor, acknowledgment_date__isnull=False).values_list('acknowledgment_date', 'issue_date')
+        average_response_time = sum((ack_date - issue_date ).total_seconds() for ack_date, issue_date in response_times)   #/ len(response_times)
+        if average_response_time < 0:
+            average_response_time = 0
+        if response_times:
+            average_response_time = average_response_time / len(response_times)
+        else:
+            average_response_time = 0  # Avoid division by zero if there are no response times
+        instance.vendor.average_response_time = average_response_time
+        instance.vendor.save()
+
+@receiver(post_save, sender=PurchaseOrder)
+def update_fulfillment_rate(sender, instance, **kwargs):
+    # Update Fulfillment Rate
+    fulfilled_orders = PurchaseOrder.objects.filter(vendor=instance.vendor, status='completed')  #, quality_rating__isnull=False)
+    fulfillment_rate = fulfilled_orders.count() / PurchaseOrder.objects.filter(vendor=instance.vendor).count()
+    instance.vendor.fulfillment_rate = fulfillment_rate
+    instance.vendor.save()
--- vendor/vendorsapp/views.py
+++ vendor/vendorsapp/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
-# Create your views here.
-from rest_framework import generics
-from rest_framework.response import Response
+from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import *
 from .models import *
 from .serializers import *
-
+from rest_framework.response import Response
+from django.utils import timezone
+from django.db.models import Avg
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics, status
 
 @api_view(['GET'])
 def index(request):
@@ -46,5 +48,16 @@ class AcknowledgePurchaseOrderAPIView(generics.UpdateAPIView):
     queryset = PurchaseOrder.objects.all()
     serializer_class = PurchaseOrderSerializer
 
-    def perform_update(self, serializer):
-        instance = serializer.save(acknowledgment_date=timezone.now())
+    def perform_update(self, request, *args, **kwargs):
+        instance = self.get_object() 
+        instance.acknowledgment_date = request.data.get('acknowledgment_date')      
+        instance.save()
+        response_times = PurchaseOrder.objects.filter(vendor=instance.vendor, acknowledgment_date__isnull=False).values_list('acknowledgment_date', 'issue_date')
+        average_response_time = sum(abs((ack_date - issue_date).total_seconds()) for ack_date, issue_date in response_times) #/ len(response_times)
+        if response_times:
+            average_response_time = total_seconds / len(response_times)
+        else:
+            average_response_time = 0  # Avoid division by zero if there are no response times
+        instance.vendor.average_response_time = average_response_time
+        instance.vendor.save()
+        return Response({'acknowledgment_date': instance.acknowledgment_date})
